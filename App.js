b11ca8c4cb646d728c5199386e0e8cf96c859371
Debanu Gupta <debanugupta@gmail.com>
2021-11-27 19:21:50 +0530

    152. Redux & Store Setup

--- App.js
+++ App.js
@@ -3,10 +3,19 @@ import { Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
 import { enableScreens } from 'react-native-screens';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
 import MealsNavigator from './navigation/MealsNavigator';
+import mealsReducer from './store/reducers/meals';
 
 enableScreens();
+
+const rootReducer = combineReducers({
+  meals: mealsReducer
+});
+
+const store = createStore(rootReducer);
 
 const fetchFonts = () => {
   return Font.loadAsync({
@@ -21,12 +30,16 @@ export default function App() {
   if (!fontLoaded) {
     return (
       <AppLoading
-    startAsync={fetchFonts}
-    onFinish={() => setFontLoaded(true)}
-    onError={(err) => console.log(err)}
-/>
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
     );
   }
 
-  return <MealsNavigator />;
+  return (
+    <Provider store={store}>
+      <MealsNavigator />
+    </Provider>
+  );
 }
